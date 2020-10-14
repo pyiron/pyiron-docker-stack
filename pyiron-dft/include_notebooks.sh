85364a13081659ef9b0f8c96026bd196c27cdbcf
muh-hassani <62968151+muh-hassani@users.noreply.github.com>
2020-10-15 01:02:03 +0200

    fixing the syntax error in inlude_notebook.sh

--- pyiron-dft/include_notebooks.sh
+++ pyiron-dft/include_notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/pyiron/pyiron
-mv "$HOME"/pyiron/notebooks/ "$HOME"/
+cp -r "$HOME"/pyiron/notebooks/ "$HOME"/
 rm -r "$HOME"/pyiron
 for f in $(cat "$HOME"/exclude); do rm "$HOME"/notebooks/$f; done;
