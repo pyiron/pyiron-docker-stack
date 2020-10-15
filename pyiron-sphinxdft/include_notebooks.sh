9e33409fd974bc5c2bf28dfe1b9af9b7488b00d7
muh-hassani <62968151+muh-hassani@users.noreply.github.com>
2020-10-15 03:19:03 +0200

    cleaning up dockerfiles

--- pyiron-sphinxdft/include_notebooks.sh
+++ pyiron-sphinxdft/include_notebooks.sh
@@ -3,3 +3,7 @@ git clone https://github.com/pyiron/pyiron
 cp -r "$HOME"/pyiron/notebooks/ "$HOME"/
 rm -r "$HOME"/pyiron
 for f in $(cat "$HOME"/exclude); do rm "$HOME"/notebooks/$f; done;
+rm "$HOME"/*.yml
+rm "$HOME"/Dockerfile
+rm "$HOME"/exclude
+rm "$HOME"/*.sh
