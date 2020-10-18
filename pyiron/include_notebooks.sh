6dd378dcb72e451d08e4c40676b9613da7d3431e
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-10-18 16:36:32 +0200

    Update include_notebooks.sh

--- pyiron/include_notebooks.sh
+++ pyiron/include_notebooks.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/pyiron/pyiron
 cp -r "$HOME"/pyiron/notebooks/*.ipynb "$HOME"/
 rm -r "$HOME"/pyiron
-for f in $(cat "$HOME"/exclude); do rm "$HOME"/notebooks/$f; done;
+for f in $(cat "$HOME"/exclude); do rm "$HOME"/$f; done;
 rm "$HOME"/*.yml
 rm "$HOME"/Dockerfile
 rm "$HOME"/exclude
