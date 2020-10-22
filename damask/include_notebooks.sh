06e028b773732d641d0d04a96c4ee77006a0d140
muh-hassani <muhammad@cmleo14.mpie.de>
2020-10-22 14:35:38 +0200

    updating damask/include_notebooks.sh

--- damask/include_notebooks.sh
+++ damask/include_notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/matbinder/DAMASK-example
-cp "$HOME"/DAMASK-example/DAMASK_workflows.ipynb "$HOME"/
+cp "$HOME"/DAMASK-example/DAMASK_WORKFLOWS.ipynb "$HOME"/
 cp "$HOME"/DAMASK-example/*.py "$HOME"/
 rm -r "$HOME"/DAMASK-example
 rm "$HOME"/*.yml
