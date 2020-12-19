c651593de25337b30c546460129ffed3a07b1721
muh-hassani <muhammad@cmleo14.mpie.de>
2020-12-19 09:27:34 +0100

    updating fenics/include_noteboks.sh

--- fenics/include_notebooks.sh
+++ fenics/include_notebooks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git clone https://github.com/pyiron/pyiron_contrib
+git clone https://github.com/pyiron/pyiron_contrib "$HOME"/from_contrib
+cd "$HOME"/from_contrib/
 git checkout FEniCS_job
-cp -r "$HOME"/pyiron/notebooks/fenics_tutorial.ipynb "$HOME"/
-cp -r "$HOME"/pyiron_contrib/pyiron_contrib "$HOME/"
+cp "$HOME"/from_contrib/notebooks/fenics_tutorial.ipynb "$HOME"/
+cp -r "$HOME"/from_contrib/pyiron_contrib "$HOME"/
+rm -r "$HOME"/from_contrib
 rm "$HOME"/*.yml
 rm "$HOME"/Dockerfile
-rm "$HOME"/exclude
 rm "$HOME"/*.sh
