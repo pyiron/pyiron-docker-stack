35e0d195b298f209eb75433594f088dac1075864
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-12-23 22:27:26 +0100

    Update include_notebooks.sh

--- fenics/include_notebooks.sh
+++ fenics/include_notebooks.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/pyiron/pyiron_contrib "$HOME"/pyiron_contrib/
 cd "$HOME"/pyiron_contrib/
 git checkout FEniCS_job
-pip install .
+pip install --no-deps .
 cp "$HOME"/pyiron_contrib/notebooks/fenics_tutorial.ipynb "$HOME"/
 cd ..
 rm -r "$HOME"/pyiron_contrib
