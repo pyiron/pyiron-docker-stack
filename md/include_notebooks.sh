83a0d63d5d802b80142b9f3a45c2978fbe7eef63
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-01-16 18:43:14 +0100

    Fix copy

--- md/include_notebooks.sh
+++ md/include_notebooks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/pyiron/pyiron_atomistics
-cp -r "$HOME"/pyiron/notebooks/*.ipynb "$HOME"/
-rm -r "$HOME"/pyiron
+cp -r "$HOME"/pyiron_atomistics/notebooks/*.ipynb "$HOME"/
+rm -r "$HOME"/pyiron_atomistics
 for f in $(cat "$HOME"/exclude); do rm "$HOME"/$f; done;
 rm "$HOME"/*.yml
 rm "$HOME"/Dockerfile
