d0c6414134723745086eab446adfb5d6068e5ccc
muh-hassani <mhassani86@gmail.com>
2021-03-23 17:15:28 +0100

    adding continuum/include_notebooks.sh

---
+++ continuum/include_notebooks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone https://github.com/pyiron/pyiron_continuum "$HOME"/pyiron_continuum/
+cd "$HOME"/pyiron_continuum/
+cp "$HOME"/pyiron_continuum/notebooks/*.ipynb "$HOME"/
+cd ..
+git clone https://github.com/materialdigital/pyiron-workflow-damask "$HOME"/pyiron-workflow-damask/
+cd "$HOME"/pyiron-workflow-damask/
+cp "$HOME"/pyiron-workflow-damask/*.ipynb "$HOME"/
+cp -r "$HOME"/pyiron-workflow-damask/Examples/ "$HOME"/
+cd ..
+rm -r "$HOME"/pyiron_continuum
+rm -r "$HOME"/pyiron-workflow-damask/
+rm "$HOME"/*.yml
+rm "$HOME"/Dockerfile
+rm "$HOME"/*.sh
