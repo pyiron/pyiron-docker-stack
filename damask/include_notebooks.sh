0d02b8695a0e1833c08c4f472520b15e2cde3977
muh-hassani <mhassani86@gmail.com>
2021-03-23 17:14:01 +0100

    removal of damask and fenics images

--- damask/include_notebooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/matbinder/DAMASK-example
-cp "$HOME"/DAMASK-example/DAMASK_WORKFLOWS.ipynb "$HOME"/
-cp -r "$HOME"/DAMASK-example/* "$HOME"/
-rm -r "$HOME"/DAMASK-example
-rm "$HOME"/*.yml
-rm "$HOME"/Dockerfile
-rm "$HOME"/*.sh
