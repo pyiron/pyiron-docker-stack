8101e71ba872dd01b5d266ac3fdf8b0ccf9093fd
muh-hassani <mhassani86@gmail.com>
2021-03-24 11:04:45 +0100

    updating continuum/include_notebooks.sh

--- continuum/include_notebooks.sh
+++ continuum/include_notebooks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/pyiron/pyiron_continuum "$HOME"/pyiron_continuum/
 cd "$HOME"/pyiron_continuum/
-cp "$HOME"/pyiron_continuum/notebooks/*.ipynb "$HOME"/
+cp "$HOME"/pyiron_continuum/notebooks/fenics_tutorial.ipynb "$HOME"/
 cd ..
 git clone https://github.com/materialdigital/pyiron-workflow-damask "$HOME"/pyiron-workflow-damask/
 cd "$HOME"/pyiron-workflow-damask/
