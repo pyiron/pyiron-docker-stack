b62e66d290ed251c1018f5ae5dd585182ddc805b
muh-hassani <mhassani86@gmail.com>
2021-11-08 11:04:57 +0100

    updating continuum notebook script

--- continuum/include_notebooks.sh
+++ continuum/include_notebooks.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 git clone https://github.com/pyiron/pyiron_continuum "$HOME"/pyiron_continuum/
 cp "$HOME"/pyiron_continuum/notebooks/fenics_tutorial.ipynb "$HOME"/
-cp "$HOME"/pyiron_continuum/notebooks/damask_first_steps.ipynb "$HOME"/
-cp -r "$HOME"/pyiron_continuum/notebooks/damask_inputs "$HOME"/
+cp "$HOME"/pyiron_continuum/notebooks/damask_tutorial.ipynb "$HOME"/
 
 rm -r "$HOME"/pyiron_continuum
 rm "$HOME"/*.yml
