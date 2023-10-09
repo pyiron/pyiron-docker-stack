1d51b0117df8409c1bf14e233308633fd94af593
mbruns91 <marianbruns91@gmail.com>
2023-10-09 16:30:39 +0200

    renamde 'include_notebooks.sh' to 'aggregate_and_clean.sh'

--- md/aggregate_and_clean.sh
+++ md/aggregate_and_clean.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# aggegate notebook from the pyiron_atomistics repo into this image
 git clone https://github.com/pyiron/pyiron_atomistics
 cp -r "$HOME"/pyiron_atomistics/notebooks/*.ipynb "$HOME"/
-rm -r "$HOME"/pyiron_atomistics
+
+# exclude notebooks listed in file 'exclude'
 for f in $(cat "$HOME"/exclude); do rm "$HOME"/$f; done;
+rm "$HOME"/exclude
+
+# clean up the image's $HOME
+rm -r "$HOME"/pyiron_atomistics
 rm "$HOME"/*.yml
 rm "$HOME"/Dockerfile
-rm "$HOME"/exclude
 rm "$HOME"/*.sh
