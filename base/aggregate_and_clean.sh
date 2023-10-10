f19338745a71e401130325fc7f6e85fc3ce67e69
mbruns91 <marianbruns91@gmail.com>
2023-10-10 16:38:45 +0200

    reiterated

--- base/aggregate_and_clean.sh
+++ base/aggregate_and_clean.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # aggegate notebook from the pyiron_base repo into this image
-git clone https://github.com/pyiron/pyiron_base
+git clone https://github.com/pyiron/pyiron_base.git
 cp -r "$HOME"/pyiron_base/notebooks/*.ipynb "$HOME"/
 
 # clean up the image's $HOME
 rm -r "$HOME"/pyiron_base
-rm "$HOME"/*.yml
+rm "$HOME"/environment.yml
 rm "$HOME"/Dockerfile
-rm "$HOME"/*.sh
+rm "$HOME"/apt.txt
+rm "$HOME"/aggregate_and_clean.sh
