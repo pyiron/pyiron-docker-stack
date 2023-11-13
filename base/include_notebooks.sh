82a5917891e7ca4c190406277d4148882d18a162
mbruns91 <m.bruns@mpie.de>
2023-11-13 16:43:07 +0100

    modified 'include_notebooks.sh' to move notebooks into one sub-dir

--- base/include_notebooks.sh
+++ base/include_notebooks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+NOTEBOOK_DIR="${HOME}"/notebooks_base
 git clone https://github.com/pyiron/pyiron_base
-cp -r "$HOME"/pyiron_base/notebooks/*.ipynb "$HOME"/
+cp -r "$HOME"/pyiron_base/notebooks/*.ipynb "$NOTEBOOK_DIR"
 rm -r "$HOME"/pyiron_base
 rm "$HOME"/*.yml
 rm "$HOME"/Dockerfile
