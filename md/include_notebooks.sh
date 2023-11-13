5140f50cc3628d6cc7a5e097e9af958a423fa6f3
mbruns91 <m.bruns@mpie.de>
2023-11-13 17:00:39 +0100

    modified 'include_notebooks.sh'

--- md/include_notebooks.sh
+++ md/include_notebooks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+NOTEBOOK_DIR="${HOME}"/notebooks_atomistics
+mkdir ${NOTEBOOK_DIR}
 git clone https://github.com/pyiron/pyiron_atomistics
-cp -r "$HOME"/pyiron_atomistics/notebooks/*.ipynb "$HOME"/
+cp -r "$HOME"/pyiron_atomistics/notebooks/*.ipynb "$NOTEBOOK_DIR"
 rm -r "$HOME"/pyiron_atomistics
 for f in $(cat "$HOME"/exclude); do rm "$HOME"/$f; done;
 rm "$HOME"/*.yml
