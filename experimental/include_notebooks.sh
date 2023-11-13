ca434e10865df7c1ab3f8b3690785c3967f5ccf4
mbruns91 <m.bruns@mpie.de>
2023-11-13 18:29:41 +0100

    cleaned HOME of experimental image

--- experimental/include_notebooks.sh
+++ experimental/include_notebooks.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+mkdir ${NOTEBOOK_DIR} 
 git clone https://github.com/pyiron/pyiron_experimental pyiron_experimental_repo
-cp "$HOME"/pyiron_experimental_repo/notebooks/*.ipynb "$HOME"/
-cp "$HOME"/pyiron_experimental_repo/notebooks/*.tif "$HOME"/
-cp "$HOME"/pyiron_experimental_repo/notebooks/*.emd "$HOME"/
-cp -r "$HOME"/pyiron_experimental_repo/pyiron_experimental "$HOME"/
-rm -r "$HOME"/pyiron_experimental_repo
+cp "$HOME"/pyiron_experimental_repo/notebooks/*.ipynb "${NOTEBOOK_DIR}"
+cp "$HOME"/pyiron_experimental_repo/notebooks/*.tif "${NOTEBOOK_DIR}"
+cp "$HOME"/pyiron_experimental_repo/notebooks/*.emd "${NOTEBOOK_DIR}"
+cp -r "$HOME"/pyiron_experimental_repo/pyiron_experimental "${NOTEBOOK_DIR}"
+rm -rf "$HOME"/pyiron_experimental_repo
 rm "$HOME"/Dockerfile
 rm "$HOME"/*.sh
 rm "$HOME"/*.yml
