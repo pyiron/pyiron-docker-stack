b07ff9e7d3af500d46a87a60de8193cf74160f36
mbruns91 <m.bruns@mpie.de>
2023-11-13 19:18:45 +0100

    fix typo

--- continuum/include_notebooks.sh
+++ continuum/include_notebooks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git clone https://github.com/pyiron/pyiron_continuum "$HOME"/pyiron_continuum/
-cp "$HOME"/pyiron_continuum/notebooks/fenics_tutorial.ipynb "$HOME"/
-cp "$HOME"/pyiron_continuum/notebooks/damask_tutorial.ipynb "$HOME"/
-
-rm -r "$HOME"/pyiron_continuum
-rm "$HOME"/*.yml
-rm "$HOME"/Dockerfile
-rm "$HOME"/*.sh
+mkdir ${NOTEBOOK_DIR}
+git clone https://github.com/pyiron/pyiron_continuum
+cp "${HOME}"/pyiron_continuum/notebooks/fenics_tutorial.ipynb "${NOTEBOOK_DIR}"
+cp "${HOME}"/pyiron_continuum/notebooks/damask_tutorial.ipynb "${NOTEBOOK_DIR}"
+rm -r "${HOME}"/pyiron_continuum
+rm "${HOME}"/*.yml
+rm "${HOME}"/Dockerfile
+rm "${HOME}"/*.sh
