fcfe3cd52c696ab9de72824c66d278fb9637d293
mbruns91 <m.bruns@mpie.de>
2023-11-13 19:24:51 +0100

    cleaned HOME of continuum image

--- continuum/include_notebooks.sh
+++ continuum/include_notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ${NOTEBOOK_DIR}
-git clone https://github.com/pyiron/pyiron_continuum
+git clone https://github.com/pyiron/pyiron_continuum.git
 cp "${HOME}"/pyiron_continuum/notebooks/fenics_tutorial.ipynb "${NOTEBOOK_DIR}"
 cp "${HOME}"/pyiron_continuum/notebooks/damask_tutorial.ipynb "${NOTEBOOK_DIR}"
 rm -r "${HOME}"/pyiron_continuum
