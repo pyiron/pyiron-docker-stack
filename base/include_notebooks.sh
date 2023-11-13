8e6da7b21f6803b86b76e00b25f1b8c7d9d1a2ad
mbruns91 <m.bruns@mpie.de>
2023-11-13 17:21:12 +0100

    moved NOTEBOOK_DIR variable to Dockerfile

--- base/include_notebooks.sh
+++ base/include_notebooks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-NOTEBOOK_DIR="${HOME}"/notebooks_base
 mkdir ${NOTEBOOK_DIR}
 git clone https://github.com/pyiron/pyiron_base
 cp -r "$HOME"/pyiron_base/notebooks/*.ipynb "$NOTEBOOK_DIR"
