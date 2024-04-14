f041192f798ba27bd54d6d8b7844d9b5d342d928
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-14 07:33:44 -0500

    Update include_notebooks.sh

--- pyiron/include_notebooks.sh
+++ pyiron/include_notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ${NOTEBOOK_DIR}
-git clone https://github.com/pyiron/pyiron
+git clone --single-branch --branch pyiron-$(grep pyiron environment.yml | tr "=" "\n" | sed '$!d')  https://github.com/pyiron/pyiron.git
 cp -r "${HOME}"/pyiron/notebooks/*.ipynb "${NOTEBOOK_DIR}"
 rm -r "${HOME}"/pyiron
 for f in $(cat "${HOME}"/exclude); do rm "${NOTEBOOK_DIR}"/$f; done;
